363c3312b46b69dc3bb61266c900e0f8bcac7782
Ruben Sanduleac <ruvim.sanduleac@gmail.com>
2022-03-10 11:38:33 -0800

    added the puppy gif

--- main.py
+++ main.py
@@ -9,7 +9,9 @@ def hello_world():
            "<p style='color: blue' align='center'>This is a paragraph</p>" \
            "<img src='https://www.comfortzone.com/-/media/Images/ComfortZone-NA/US/Blog/bringing-new-kitten-home.jpg" \
            "?h=667&la=en&w=1000&hash=1B30399EA623982F3A1BCF8365A4864BB329CC70' " \
-           "width='300'>"
+           "width='300'>" \
+           "<img src='https://media3.giphy.com/media/aVPS1Ek7RhTtS/giphy.gif" \
+           "?cid=790b7611866ab6c444bb5317b1bbd0ab3cfca5eb4062f2fb&rid=giphy.gif&ct=g'>"
 
 @app.route("/<name>/<int:number>")
 def greet(name, number):
