91f2e40e61517c4a51306c4fb97fc7b7f7727e72
Ruben Sanduleac <ruvim.sanduleac@gmail.com>
2022-03-10 11:49:23 -0800

    implemented free wrapper functions used to italics, bold and underline

--- main.py
+++ main.py
@@ -3,7 +3,31 @@ from flask import Flask
 app = Flask(__name__)
 
 
+def bold(function):
+    def wrapper():
+        return "<b>" + function() + "</b>"
+
+    return wrapper
+
+
+def italics(function):
+    def wrapper():
+        return "<em>" + function() + "</em>"
+
+    return wrapper
+
+
+def underline(function):
+    def wrapper():
+        return "<u>" + function() + "</u>"
+
+    return wrapper
+
+
 @app.route("/")
+@italics
+@bold
+@underline
 def hello_world():
     return "<h1 style='color: red' align='center'>Hello, World!</h1>" \
            "<p style='color: blue' align='center'>This is a paragraph</p>" \
@@ -12,6 +36,12 @@ def hello_world():
            "width='300'>" \
            "<img src='https://media3.giphy.com/media/aVPS1Ek7RhTtS/giphy.gif" \
            "?cid=790b7611866ab6c444bb5317b1bbd0ab3cfca5eb4062f2fb&rid=giphy.gif&ct=g'>"
+
+
+@app.route("/bye")
+def bye():
+    pass
+
 
 @app.route("/<name>/<int:number>")
 def greet(name, number):
