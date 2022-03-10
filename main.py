84cf194fdaf1ce3b8d8580868229843b2136d778
Ruben Sanduleac <ruvim.sanduleac@gmail.com>
2022-03-10 11:23:01 -0800

    using variables with flask

--- main.py
+++ main.py
@@ -2,10 +2,15 @@ from flask import Flask
 
 app = Flask(__name__)
 
+
 @app.route("/")
 def hello_world():
     return "Rule Hello, World!"
 
+@app.route("/<name>/<int:number>")
+def greet(name, number):
+    return f"Hello {name} your age is {number}!"
+
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
