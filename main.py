5de80857756fe4815b11ff92b600e723b11ec00e
Ruben Sanduleac <ruvim.sanduleac@gmail.com>
2022-03-10 11:33:06 -0800

    aaded the image of a kitten on the homepage

--- main.py
+++ main.py
@@ -5,7 +5,11 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-    return "Rule Hello, World!"
+    return "<h1 style='color: red' align='center'>Hello, World!</h1>" \
+           "<p style='color: blue' align='center'>This is a paragraph</p>" \
+           "<img src='https://www.comfortzone.com/-/media/Images/ComfortZone-NA/US/Blog/bringing-new-kitten-home.jpg" \
+           "?h=667&la=en&w=1000&hash=1B30399EA623982F3A1BCF8365A4864BB329CC70' " \
+           "width='300'>"
 
 @app.route("/<name>/<int:number>")
 def greet(name, number):
