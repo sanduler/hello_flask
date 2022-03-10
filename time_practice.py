3dd01de8d11fef8158c0470811125a8b6d331549
Ruben Sanduleac <ruvim.sanduleac@gmail.com>
2022-03-09 16:19:42 -0800

    added comments explaining the functionality

--- time_practice.py
+++ time_practice.py
@@ -5,10 +5,16 @@ print(current_time)
 
 
 def speed_calc_decorator(function):
+    """This a wrapper function that takes the function as an input and
+    then raps the passed function with the wrapper."""
     def wrapper_function():
+        # set the start time to a variable
         start_time = time.time()
+        # function call the function that is passed
         function()
+        # get the endtime
         end_time = time.time()
+        # calculate the diffrence for the run speed for the fast function/slow function
         print(f"{function.__name__} run speed: {abs(start_time - end_time)}")
 
     return wrapper_function
@@ -16,17 +22,20 @@ def speed_calc_decorator(function):
 
 @speed_calc_decorator
 def fast_functions():
+    # user a for loop to go through a million interactions
     for i in range(10000000):
         i * i
 
 
 @speed_calc_decorator
 def slow_function():
+    # user a for loop to go through a million interactions
     for i in range(100000000):
         i * i
 
 
 def main():
+    # function call both functions
     fast_functions()
     slow_function()
 
