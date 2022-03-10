439625476ddf114acbf5a21fb7b61c7c6916a354
Ruben Sanduleac <ruvim.sanduleac@gmail.com>
2022-03-09 16:53:58 -0800

    created a decorator fucntion and implemented the sum and the differrence functions for the candy crush file

--- candy_crush.py
+++ candy_crush.py
@@ -1,13 +1,29 @@
+import time
+
+current_time = time.time()
+print(current_time)
 
 
 def decorator(function):
     def wrapper():
-        pass
+        print("Hello")
+        print(function(3, 4))
 
+    return wrapper
+
+
+@decorator
+def sum(number1, number2):
+    return number1 + number2
+
+@decorator
+def diffrence(number1, number2):
+    return number1 - number2
 
 
 def main():
-    pass
+    sum()
+    diffrence()
 
 
 if __name__ == '__main__':
